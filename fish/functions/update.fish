d673e06527d9c39af2d89db5e8c09200668012a8
Nicola Massarenti <nicola.massarenti@gmail.com>
2022-12-01 21:50:37 +0100

    Updated update function

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -1,3 +1,3 @@
-function update --wraps='sudo apt update && sudo apt upgrade && sudo snap refresh' --description 'alias update=sudo apt update && sudo apt upgrade && sudo snap refresh'
-  sudo apt update && sudo apt upgrade && sudo snap refresh $argv; 
+function update --wraps='sudo apt update && sudo apt upgrade && sudo snap refresh' --wraps='sudo apt update && sudo apt upgrade -y && sudo snap refresh' --description 'alias update=sudo apt update && sudo apt upgrade -y && sudo snap refresh'
+  sudo apt update && sudo apt upgrade -y && sudo snap refresh $argv; 
 end
